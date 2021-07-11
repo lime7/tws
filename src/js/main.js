db619b7420992cedfa115c2fc0e212605d0fb70c
lime7 <senseless.v.7@gmail.com>
2021-07-11 21:25:50 +0300

    Locomotive-scroll

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,27 @@ console.log('S>>>')
 
 // eslint-disable-next-line import/first
 import { fslightbox } from 'fslightbox'
+
+// eslint-disable-next-line import/first
+import LocomotiveScroll from 'locomotive-scroll'
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+})
+
+// Get the header
+const header = document.getElementById('js-header')
+
+// Get the offset position of the navbar
+var sticky = header.getBoundingClientRect().height
+
+scroll.on('scroll', (instance) => {
+  if (instance.scroll.y > sticky) {
+    header.classList.remove('bg-transparent')
+    header.classList.add('bg-blue-900')
+  } else {
+    header.classList.remove('bg-blue-900')
+    header.classList.add('bg-transparent')
+  }
+})
